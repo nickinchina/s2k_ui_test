71334a890251f88576547bc4b6b4ff0c9811cef3
nickinchina <shoutsoft@hotmail.com>
2015-07-11 15:35:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 var exec = require('child_process').exec;
 var moment = require('moment');
 
-var filename = moment().format('YYYY-MM-DD HH-mm-ss.SSS') + '[MRK].log';
+var filename = moment().format('YYYY-MM-DD HH-mm-ss.SSS') + '[ENT].log';
+console.log('RUNNING S2K UI TEST --- ["Entities"] MODULE ...\nPLEASE WAIT ...');
+exec('protractor s2k_entities_conf.js > "log/' + filename + '"', {
+  cwd: '/root/s2k_ui_test/bat'
+}, function(error, stdout, stderr) {
+    if ( !error ) {
+        console.log('UI TEST ["Entities"] MODULE FINISED. \nPLEASE CHECK THE LOG FILE: "' + filename + '"');
+        //console.log(stdout);
+    } else {
+        console.log('UI TEST ["Entities"] MODULE ERROR. \nPLEASE CHECK THE LOG FILE: "' + filename + '"');
+        console.log(error);
+    }
+});
+
+filename = moment().format('YYYY-MM-DD HH-mm-ss.SSS') + '[MRK].log';
 console.log('RUNNING S2K UI TEST --- ["Marketing"] MODULE ...\nPLEASE WAIT ...');
 exec('protractor s2k_marketing_conf.js > "log/' + filename + '"', {
   cwd: '/root/s2k_ui_test/bat'
